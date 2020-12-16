57646e6518e1a543e0eaef5b5c6380a7c18d97dd
ted <heinhtetnaing186@gmail.com>
2020-12-16 14:30:31 +0630

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import * as faceapi from "face-api.js";
+
+import "./app.scss";
 
 function App() {
+  const runModels = async () => {
+    await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+    await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+    await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+    await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+    await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+    streamCamVideo();
+  };
+
+  const streamCamVideo = async () => {
+    var constraints = { audio: false, video: { width: 1280, height: 720 } };
+    try {
+      let MediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+      let video = document.querySelector("video");
+      video.srcObject = MediaStream;
+      video.onloadedmetadata = (e) => {
+        video.play().then(findFace());
+      };
+    } catch (e) {
+      console.log(e.name + ": " + e.message);
+    }
+  };
+
+  const findFace = () => {
+    const video = document.getElementById("video");
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    // const displaySize = { width: video.width, height: video.height };
+    const displaySize = { width: canvas.width, height: canvas.height };
+
+    setInterval(async () => {
+      const detection = await faceapi
+        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        // .withFaceExpressions()
+        .withFaceLandmarks();
+
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      const resizedDetection = faceapi.resizeResults(detection, displaySize);
+      faceapi.draw.drawDetections(canvas, resizedDetection);
+      // faceapi.draw.drawFaceExpressions(canvas, resizedDetection);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetection);
+    }, 100);
+  };
+
+  useEffect(() => {
+    runModels();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video autoPlay id="video" />
     </div>
   );
 }
